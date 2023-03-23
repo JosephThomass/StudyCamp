e88595addd90b84f4146a840693c1c27b2db3c3a
Joseph Thomas <josephthomas88m@gmail.com>
2023-03-23 23:48:15 +0530

    Django setup initialized

---
+++ studycamp/base/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home page") 
+
+def rooms(request):
+    return HttpResponse("This is a study room")
