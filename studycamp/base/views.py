89db9dd787d31d0be967be2bdd76a6e956010517
Joseph Thomas <josephthomas88m@gmail.com>
2023-04-06 20:16:56 +0530

    git ignore

--- studycamp/base/views.py
+++ studycamp/base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
+rooms=[
+    {'id':1,'name':'Python'},
+    {'id':2,'name':'Html'},
+    {'id':3,'name':'Css'},
+    {'id':4,'name':'JavaScript'}    
+    ]
+
 def home(request):
-    return render(request, 'home.html')
+    context={'rooms':rooms}
+    return render(request, 'home.html',context)
 
 def rooms(request):
     return render(request, 'rooms.html')
